c15790bfab032d3cf80367366277230476f86598
Robiul Hossain <munshearfat4@gmail.com>
2024-04-02 02:25:21 +0600

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -26,13 +26,13 @@ def grab(url):
         #response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('https://arafatplay.top/Video/livestream.m3u8')
+                print('https://arafatplay.top/Video/livestream2.m3u8')
                 return
             #os.system(f'wget {url} -O temp.txt')
             os.system(f'curl "{url}" > temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('https://arafatplay.top/Video/livestream.m3u8')
+                print('https://arafatplay.top/Video/livestream2.m3u8')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100
